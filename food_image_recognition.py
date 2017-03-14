98b31f92167f93b468275022c73e780b0261568d
Suhas Nithyanandappa <suhas.nithyanand@gmail.com>
2017-03-14 03:11:36 -0400

    code cleanup

--- food_image_recognition.py
+++ food_image_recognition.py
@@ -1,5 +1,3 @@
-import math
-#from sklearn import svm
 import numpy as np
 import cv2
 from string import digits
@@ -29,8 +27,6 @@ for l in lines:
     vector = l
     vals= vector.split()
     each_feature = []
-    # print 'vector', l
-    # print 'vals', vals
     vectors_list.append(vals)
 
 
@@ -72,21 +68,10 @@ output_label = np.asarray(output_label)
 test_inputs = vectors[90000:101000]
 test_ground_truth = output_label_list[90000:101000]
 
-#print len(input_label_list)
-#print len(l1)
+
 print 'test_inputs', len(test_inputs)
-#print 'input test_ground_truth ',test_ground_truth
 print 'test ground truth', len(test_ground_truth)
 
-
-#
-# svm = cv2.ml.SVM_create()
-# svm.setKernel(cv2.ml.SVM_LINEAR)
-# svm.setP(0.2)
-# svm.setType(cv2.ml.SVM_EPS_SVR)
-# #svm.setTermCriteria((cv2.TERM_CRITERIA_COUNT, 100, 1.e-06))
-# svm.setC(12.5)
-# svm.setGamma(5.383)
 
 x = input_vectors.astype(np.float32, copy=False)
 y = output_label.astype(np.float32, copy=False)
@@ -108,47 +93,10 @@ print 'x shape', np.shape(x)
 print 'y shape',np.shape(y)
 
 
-''' Training using svm algorithm using opencv library '''
-
-
-#svm.train(x,y, params=svm_params)
-# svm.train(x, cv2.ml.ROW_SAMPLE, y)
-
-
-
 ''' Testing '''
-#test_result_list = []
-#for t in range(len(test_inputs)):
-#	#test = np.float32(test_inputs[t])
-#	test_result = svm.predict(test)
-#	test_result_list.append(test_result)
-
-#test_data = np.array(test_inputs, np.float32)
-
 test_result_list = clf.predict(test_inputs)
-
-
-# test_result_list = svm.predict(test_data)
 print 'test_result',test_result_list
-#print 'test_result', len(test_result_list)
-#print 'test_result', len(test_result_list[1].tolist())
 
-# test_res = test_result_list[1].tolist()
-# # print 'test res len',len(test_res)
-# print 'test res', test_res
-# # print '-----------------'
-# # print 'test ground truth', test_ground_truth
-#
-# final = []
-# for i in test_res:
-# 	final.append(int(round(i[0])))
-#
-# print 'final',final
-#
-# #print 'checking output'
-# #for i,j in zip(final,test_ground_truth):
-# #	print 'prediction:', i, '\t','ground truth:',j
-#
 '''evaluating metrics'''
 accuracy = accuracy_score(test_ground_truth, test_result_list)
 f1_score = f1_score(test_ground_truth,test_result_list, average='macro')
